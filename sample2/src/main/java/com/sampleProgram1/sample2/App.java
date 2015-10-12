130fdb4969a32cd2b86606cc4490bfbbfd4f6b3c
pulis0213 <sindhu.puli@gmail.com>
2015-10-12 12:40:50 -0500

    sample2 commit

---
+++ sample2/src/main/java/com/sampleProgram1/sample2/App.java
@@ -0,0 +1,13 @@
+package com.sampleProgram1.sample2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
